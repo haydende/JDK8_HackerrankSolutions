c4ab373a49467907cd48a58a08ddce2da67a37e2
haydende <haydeneastwell@outlook.com>
2023-05-15 22:02:42 +0100

    Initial JavaNegativeSubArray.java

---
+++ src/main/java/haydende/JavaNegativeSubArray.java
@@ -0,0 +1,15 @@
+package haydende;
+
+/**
+ * https://www.hackerrank.com/challenges/java-negative-subarray/problem
+ */
+public class JavaNegativeSubArray {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int countNegativeSubs(String[] inputArr) {
+        return 0;
+    }
+}
